b85d74bffafec1ad37693c283776f4d128d79b96
axel584 <axel584@gmail.com>
2015-04-11 17:00:25 +0200

    ajout du webservice

--- app/src/main/java/org/esperanto_france/samopiniuloj/EniriActivity.java
+++ app/src/main/java/org/esperanto_france/samopiniuloj/EniriActivity.java
@@ -1,15 +1,136 @@
 package org.esperanto_france.samopiniuloj;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import android.util.Log;
 
 public class EniriActivity extends Activity {
+
+    Button btnLogin;
+
+    EditText inputEnirnomo;
+    EditText inputPasvorto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eniri);
 
+        inputEnirnomo = (EditText) findViewById(R.id.enirnomo);
+        inputPasvorto = (EditText) findViewById(R.id.pasvorto);
+        btnLogin = (Button) findViewById(R.id.btn_eniru);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View view) {
+                String sEnirnomo = inputEnirnomo.getText().toString();
+                String sPasvorto = inputPasvorto.getText().toString();
+                if (
+                        (!sEnirnomo.equals("")) && (!sPasvorto.equals(""))) {
+                    new HttpAsyncTask().execute("http://samopiniuloj.esperanto-jeunes.org/ws/ws_eniri.php?nomo=" + sEnirnomo + "&pasvorto=" + sPasvorto);
+                } else if ((!sEnirnomo.equals(""))) {
+                    Toast.makeText(getApplicationContext(),
+                            "Password field empty", Toast.LENGTH_SHORT).show();
+                } else if ((!sPasvorto.equals(""))) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enirnomo field empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Email and Password field are empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
 
     }
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!" + result, Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
