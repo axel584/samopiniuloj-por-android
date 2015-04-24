08a75642e7ea3e000b6c3dde601d467ebaf64557
axel584 <axel584@gmail.com>
2015-04-24 23:52:29 +0200

    ajout du webservice

--- app/src/main/java/org/esperanto_france/samopiniuloj/EniriActivity.java
+++ app/src/main/java/org/esperanto_france/samopiniuloj/EniriActivity.java
@@ -1,13 +1,156 @@
 package org.esperanto_france.samopiniuloj;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
+import org.esperanto_france.samopiniuloj.modelo.EniriGson;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import android.util.Log;
+
+import com.google.gson.Gson;
 
 public class EniriActivity extends Activity {
+
+    Button btnLogin;
+
+    EditText inputEnirnomo;
+    EditText inputPasvorto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eniri);
+
+        inputEnirnomo = (EditText) findViewById(R.id.enirnomo);
+        inputPasvorto = (EditText) findViewById(R.id.pasvorto);
+        btnLogin = (Button) findViewById(R.id.btn_eniru);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View view) {
+                String sEnirnomo = inputEnirnomo.getText().toString();
+                String sPasvorto = inputPasvorto.getText().toString();
+                if (
+                        (!sEnirnomo.equals("")) && (!sPasvorto.equals(""))) {
+                    new HttpAsyncTask().execute("http://samopiniuloj.esperanto-jeunes.org/ws/ws_eniri.php?nomo=" + sEnirnomo + "&pasvorto=" + sPasvorto);
+                } else if ((!sEnirnomo.equals(""))) {
+                    Toast.makeText(getApplicationContext(),
+                            "Password field empty", Toast.LENGTH_SHORT).show();
+                } else if ((!sPasvorto.equals(""))) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enirnomo field empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Email and Password field are empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+
     }
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Gson gson = new Gson();
+            EniriGson eniriGson = gson.fromJson(result,EniriGson.class);
+            if ("eraro".equals(eniriGson.getRespondo())) {
+                // TODO : cas en erreur
+            }
+            if ("ok".equals(eniriGson.getRespondo())) {
+                // on enregistre dans sharedpreference l'id du joueur
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("SamAgordo", 0); // 0 - for private mode
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putInt("uzanto_id", eniriGson.getId());
+                editor.putInt("uzanto_nomo", eniriGson.getId());
+                editor.commit();
+                Intent mainActivity = new Intent(EniriActivity.this, MainActivity.class);
+                startActivity(mainActivity);
+            }
+
+        }
+    }
+
 }
---
+++ app/src/main/java/org/esperanto_france/samopiniuloj/modelo/EniriGson.java
@@ -0,0 +1,34 @@
+package org.esperanto_france.samopiniuloj.modelo;
+
+public class EniriGson {
+
+
+
+    private String respondo;
+    private String kialo;
+    private int id;
+
+    public String getRespondo() {
+        return respondo;
+    }
+
+    public void setRespondo(String respondo) {
+        this.respondo = respondo;
+    }
+
+    public String getKialo() {
+        return kialo;
+    }
+
+    public void setKialo(String kialo) {
+        this.kialo = kialo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
