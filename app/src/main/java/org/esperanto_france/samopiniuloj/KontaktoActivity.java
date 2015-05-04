c18d0d6b78e64ea361ad081e9e75492f555a1b07
emmrichard <erichard@universalis.fr>
2015-05-04 22:52:58 +0200

    ajout activité Kontakto

---
+++ app/src/main/java/org/esperanto_france/samopiniuloj/KontaktoActivity.java
@@ -0,0 +1,17 @@
+package org.esperanto_france.samopiniuloj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class KontaktoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kontakto);
+
+
+
+    }
+}
