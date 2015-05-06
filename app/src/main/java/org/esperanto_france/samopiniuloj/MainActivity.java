b7038d5b056c995643e230e54173b1992b2fc155
axel584 <axel584@gmail.com>
2015-05-06 22:52:59 +0200

    debut du menu hamburger

--- app/src/main/java/org/esperanto_france/samopiniuloj/MainActivity.java
+++ app/src/main/java/org/esperanto_france/samopiniuloj/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_malnova);
 
 
         bEniri = (Button) findViewById(R.id.button_enirejo);
