7771edb4486b84128334bb3f1cab785f5e306197
emmrichard <erichard@universalis.fr>
2015-05-07 23:06:49 +0200

    affichage toolbar dans la page Kiel ludi

--- app/src/main/java/org/esperanto_france/samopiniuloj/KielLudiActivity.java
+++ app/src/main/java/org/esperanto_france/samopiniuloj/KielLudiActivity.java
@@ -2,6 +2,7 @@ package org.esperanto_france.samopiniuloj;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,10 @@ public class KielLudiActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiel_ludi);
+
+        // Ajout toolbar
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
