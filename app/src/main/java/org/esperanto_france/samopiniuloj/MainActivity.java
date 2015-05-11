14c36f5431dd584329bd66e29a0cd6244c6dc8b5
emmrichard <erichard@universalis.fr>
2015-05-11 22:08:28 +0200

    suppression de l'affichage du menu Settings ("kebab" menu)

--- app/src/main/java/org/esperanto_france/samopiniuloj/MainActivity.java
+++ app/src/main/java/org/esperanto_france/samopiniuloj/MainActivity.java
@@ -138,7 +138,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // ER: j'ai mis en commentaire la ligne ci-dessous pour supprimer l'affichage du menu kebab
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
