27b372755509c345e080403a4d7257c2d1cfcc9d
emmrichard <erichard@universalis.fr>
2015-05-14 23:39:09 +0200

    ajout bouton LUDU sur la page "kiel ludi" (#46)

--- app/src/main/java/org/esperanto_france/samopiniuloj/KielLudiFragment.java
+++ app/src/main/java/org/esperanto_france/samopiniuloj/KielLudiFragment.java
@@ -1,19 +1,38 @@
 package org.esperanto_france.samopiniuloj;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class KielLudiFragment extends Fragment {
 
+    Button btnLudi;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                 Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_kiel_ludi, container, false);
+        View rootView = inflater.inflate(R.layout.activity_kiel_ludi, container, false);
 
+        btnLudi = (Button) rootView.findViewById(R.id.btn_ludu);
+
+        btnLudi.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View view) {
+                // Renvoie sur le fragment "ludi"
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, new LudiFragment())
+                        .commit();
+            }
+        });
+
+        return rootView;
     }
 
 
